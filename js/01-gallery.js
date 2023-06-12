04d3c4b8daa815c9cbfb74b47d41c1de43f72808
JoannaMikusinskaNowak <joannamikusinska@wp.pl>
2023-06-12 18:52:42 +0200

    correct 2 exercise

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,7 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+
+const findGallery = document.querySelector("#gallery");
+const imageGallery = document.createElement("li");
 
 console.log(galleryItems);
